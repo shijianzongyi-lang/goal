1c380c8d49f8f3b23c5b34ffdc233f8be5e68a5c
Junaco <shijianzongyi@gmail.com>
2026-07-01 12:35:58 +0900

    script2.js

--- script2.js
+++ script2.js
@@ -11,3 +11,15 @@ function menuchange() {
   document.querySelector('body').classList.toggle('active');
   document.querySelector('.open_menu').classList.toggle('active');
 }
+
+const htmlWidth = document.querySelector('html');
+const photoSize = htmlWidth.clientWidth * 0.8;
+for (let i = 1; i <= 4; i++) {
+  document.getElementById(`exp${i}`).setAttribute('width', photoSize);
+  document.getElementById(`exp${i}`).setAttribute('height', photoSize);
+}
+
+for (let i = 1; i <= 4; i++) {
+  document.getElementById(`cellexp${i}`).setAttribute('width', photoSize / 4);
+  document.getElementById(`cellexp${i}`).setAttribute('height', photoSize / 4);
+}
